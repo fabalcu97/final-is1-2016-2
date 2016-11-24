cee5abf910724771a769402c48bfce51c7c760d7
Angel <angel.sucapuca@ucsp.edu.pe>
2016-11-24 08:28:26 -0500

    Agtregar artista-controlador

--- src/main/java/controller/ArtistaController.java
+++ src/main/java/controller/ArtistaController.java
@@ -3,9 +3,11 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import domain.Artista;
 import repository.ArtistaRepository;
 import service.ArtistaService;
 
@@ -24,7 +26,9 @@ public class ArtistaController {
 	}
 	
 	@RequestMapping(value = "/agregar-artista", method = RequestMethod.POST)
-	String saveArtista(ModelMap model){
+	String saveArtista(@ModelAttribute Artista artista, ModelMap model){
+		artistaService.save(artista);
+		model.addAttribute("artistas", artistaService.getArtista());
 		return "agregar-artista";
 	}
 	
