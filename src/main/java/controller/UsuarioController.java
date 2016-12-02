155419b6a42ef59fe5664dbc0809fdcc063ba196
AngelJSD <angel.sucapuca@ucsp.edu.pe>
2016-12-01 20:44:33 -0500

    Modificacion de Usuario Controller

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -1,12 +1,16 @@
 package controller;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import domain.Artista;
 import domain.Usuario;
 import repository.UsuarioRepository;
 import service.UsuarioService;
@@ -19,18 +23,29 @@ public class UsuarioController {
 	@Autowired
 	UsuarioService usuarioService;
 
-	@RequestMapping(value = "/add-usuario", method = RequestMethod.POST)
-	String saveAccount(@ModelAttribute Usuario usuario, ModelMap model) {
+	@RequestMapping(value = "/usuario", method = RequestMethod.POST)
+	String saveUsuario(@ModelAttribute Usuario usuario, ModelMap model) {
+		System.out.println("saving: " + usuario.getId());
 		usuarioService.save(usuario);
-		model.addAttribute("usuarios", usuarioService.getUsuarios());
+		return showUsuario(usuario.getId(), model);
+	}
+	@RequestMapping(value = "/agregar-usuario", method = RequestMethod.GET)
+	String addNewUsuario(@RequestParam(required = false) Long id, ModelMap model) {
+		Usuario usuario = id == null ? new Usuario() : usuarioService.get(id);
+		model.addAttribute("usuario", usuario);
 		return "agregar-usuario";
 	}
-		
+
 	@RequestMapping(value = "/usuario", method = RequestMethod.GET)
-	String showUsuario(ModelMap model) {
-		model.addAttribute("usuarios", usuarioService.getUsuarios());
-		return "lista-usuarios";
+	String showUsuario(@RequestParam(required = false) Long id, ModelMap model) {
+		if (id != null) {
+			Usuario usuario = usuarioService.get(id);
+			model.addAttribute("usuario", usuario);
+			return "artista";
+		} else {
+			Collection<Usuario> usuarios = usuarioService.getAll();
+			model.addAttribute("usuarios", usuarios);
+			return "usuarios";
+		}
 	}
-	
-	
 }
--- src/main/java/service/UsuarioService.java
+++ src/main/java/service/UsuarioService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import domain.Artista;
 import domain.Usuario;
 import repository.UsuarioRepository;
 
@@ -25,7 +26,11 @@ public class UsuarioService {
 		return true;
 	}
 	
-	public Collection<Usuario> getUsuarios() {
+	public Usuario get(Long id) {
+		return usuarioRepository.find(id);
+	}
+	
+	public Collection<Usuario> getAll() {
 		return usuarioRepository.findAll();
 	}
 
